b4e5a33295daa0dac10d811fab71c7079edf4c60
Gbsyi <gbsyi@yandex.ru>
2021-09-22 21:20:27 +0700

    Final

--- Lab5/app/src/main/java/com/example/lab5/MainActivity.java
+++ Lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
+    VideoView videoPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoPlayer = (VideoView) findViewById(R.id.videoView);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.kasj);
+        videoPlayer.setVideoURI(videoUri);
+    }
+
+    public void play(View view){
+        videoPlayer.start();
+    }
+    public void pause(View view){
+        videoPlayer.pause();
+    }
+    public void stop(View view){
+        videoPlayer.stopPlayback();
+        videoPlayer.resume();
     }
 }

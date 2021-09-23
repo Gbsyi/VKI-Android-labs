8edfb75d346567574dae6f52459be70562de0a34
Gbsyi <gbsyi@yandex.ru>
2021-09-23 13:52:54 +0700

    Added Audio

--- Lab5/app/src/main/java/com/example/lab5/MainActivity.java
+++ Lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
     VideoView videoPlayer;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +19,48 @@ public class MainActivity extends AppCompatActivity {
         videoPlayer = (VideoView) findViewById(R.id.videoView);
         Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.kasj);
         videoPlayer.setVideoURI(videoUri);
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stopPlaying();
+            }
+        });
     }
+    public void startPlaying(View view){
+        if(videoPlayer.isPlaying()){
+            videoPlayer.stopPlayback();
+            videoPlayer.resume();
+        }
+        mediaPlayer.start();
+    }
+    public void stopPlaying(View view){
+        stopPlaying();
+    }
+    private void stopPlaying(){
+        mediaPlayer.stop();
+        try{
+            mediaPlayer.prepare();
+            mediaPlayer.seekTo(0);
+        }catch (Throwable t){
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(mediaPlayer.isPlaying()){
+            stopPlaying();
+        }
+    }
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
 
+    }
     public void play(View view){
+        if(mediaPlayer.isPlaying()){
+            stopPlaying();
+        }
         videoPlayer.start();
     }
     public void pause(View view){
@@ -28,4 +70,8 @@ public class MainActivity extends AppCompatActivity {
         videoPlayer.stopPlayback();
         videoPlayer.resume();
     }
+    public void playSong(View view){
+
+    }
+
 }
